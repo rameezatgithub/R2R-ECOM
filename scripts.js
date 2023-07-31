10fc2517b7f998e0a2e91aca868a37fd88e03f5a
Mohd Rameez Ansari <rameez8368@gmail.com>
2023-08-01 03:18:20 +0530

    Shop and Product pages are added (yet to resolve responsiveness of Product page).

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 const bar = document.getElementById('bar');
 const close = document.getElementById('close');
 const nav = document.getElementById('navbar');
@@ -12,4 +14,24 @@ if(close){
     close.addEventListener('click', ()=>{
         nav.classList.remove('active');
     })
+}
+
+// Image Switching
+
+var mainImage=document.getElementsByClassName("main-image");
+var smallImage=document.getElementsByClassName("small-img");
+
+// var handleClick = function(i){}
+
+smallImage[0].onclick = () => {
+    mainImage[0].src = smallImage[0].src;
+  }
+smallImage[1].onclick = () => {
+  mainImage[0].src = smallImage[1].src;
+}
+smallImage[2].onclick = () => {
+  mainImage[0].src = smallImage[2].src;
+}
+smallImage[3].onclick = () => {
+  mainImage[0].src = smallImage[3].src;
 }
